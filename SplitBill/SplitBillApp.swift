7d1cb2de0a416f929a4ba0ac824ffedfc5005305
edolubis21 <edolubis12345@gmail.com>
2024-03-12 23:30:46 +0700

    Initial Commit

---
+++ SplitBill/SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillApp.swift
+//  SplitBill
+//
+//  Created by edo lubis on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
